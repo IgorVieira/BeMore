4a36904b06c3fc7956b384e445733d7fcc1f34b4
Igor Vieira <igorpereiraramosvieira@gmail.com>
2016-08-01 13:30:36 -0300

    initial commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('input#input_text, textarea#textarea1').characterCounter();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
